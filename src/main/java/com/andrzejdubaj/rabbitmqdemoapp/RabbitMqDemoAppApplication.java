48a0d98065433e584441aa36fa1d6303d330db9a
topdeveloper1228 <topdeveloper1228@gmail.com>
2022-03-30 23:31:49 +0200

    Added listener for message

--- src/main/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMqDemoAppApplication.java
+++ src/main/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMqDemoAppApplication.java
@@ -1,26 +1,12 @@
 package com.andrzejdubaj.rabbitmqdemoapp;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitMqDemoAppApplication implements CommandLineRunner {
-
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+public class RabbitMqDemoAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitMqDemoAppApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        SimpleMessage simpleMessage = new SimpleMessage();
-        simpleMessage.setName("FirstMessage");
-        simpleMessage.setDescription("simpleDescription");
-        rabbitTemplate.convertAndSend("TestExchange", "testRouting", simpleMessage);
     }
 }
