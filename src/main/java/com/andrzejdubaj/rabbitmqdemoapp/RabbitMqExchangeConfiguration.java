6e30ee84c9f1adb17b5e3f566321dbc54815d527
topdeveloper1228 <topdeveloper1228@gmail.com>
2022-03-31 17:35:38 +0200

    Added exchanges

--- src/main/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMqExchangeConfiguration.java
+++ src/main/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMqExchangeConfiguration.java
@@ -1,10 +1,10 @@
 package com.andrzejdubaj.rabbitmqdemoapp;
 
 import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class RabbitMqExchangeConfiguration {
@@ -12,5 +12,13 @@ public class RabbitMqExchangeConfiguration {
     @Bean
     Exchange exampleExchange() {
         return new TopicExchange("ExampleExchange");
+    }
+
+    @Bean
+    Exchange example2exchange() {
+        return ExchangeBuilder.directExchange("Example2exchange")
+                .autoDelete()
+                .internal()
+                .build();
     }
 }
