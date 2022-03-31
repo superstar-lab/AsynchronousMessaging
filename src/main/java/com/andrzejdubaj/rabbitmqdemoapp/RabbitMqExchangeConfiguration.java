be1200774ed0b0fddc1a3c1a07ccdbae262a5848
topdeveloper1228 <topdeveloper1228@gmail.com>
2022-03-31 17:44:19 +0200

    Added test exchanges

--- src/main/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMqExchangeConfiguration.java
+++ src/main/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMqExchangeConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.Backoff;
 
 @Configuration
 public class RabbitMqExchangeConfiguration {
@@ -19,6 +20,33 @@ public class RabbitMqExchangeConfiguration {
         return ExchangeBuilder.directExchange("Example2exchange")
                 .autoDelete()
                 .internal()
+                .build();
+    }
+
+    @Bean
+    Exchange newExchange() {
+        return ExchangeBuilder.topicExchange("TopicTestExchange")
+                .autoDelete()
+                .durable(true)
+                .internal()
+                .build();
+    }
+
+    @Bean
+    Exchange fanoutExchange(){
+        return ExchangeBuilder.fanoutExchange("FanoutTestExchange")
+                .autoDelete()
+                .durable(false)
+                .internal()
+                .build();
+    }
+
+    @Bean
+    Exchange headersExchange(){
+        return ExchangeBuilder.headersExchange("HeadersTestExchange")
+                .internal()
+                .durable(true)
+                .ignoreDeclarationExceptions()
                 .build();
     }
 }
