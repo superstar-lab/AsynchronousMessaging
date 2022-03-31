b89bac96c1862a8bf8d0d542e7da6562e0f735ce
topdeveloper1228 <topdeveloper1228@gmail.com>
2022-03-31 18:09:05 +0200

    Deleted unused import

--- src/main/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMqExchangeConfiguration.java
+++ src/main/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMqExchangeConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.annotation.Backoff;
 
 @Configuration
 public class RabbitMqExchangeConfiguration {
