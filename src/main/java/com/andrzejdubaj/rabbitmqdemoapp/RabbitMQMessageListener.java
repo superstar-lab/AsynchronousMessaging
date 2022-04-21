7902ff0c3acfbe58f8d7f5a72e6465ff40c636ce
topdeveloper1228 <topdeveloper1228@gmail.com>
2022-04-21 19:03:38 +0200

    Code reformating

--- src/main/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMQMessageListener.java
+++ src/main/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMQMessageListener.java
@@ -3,10 +3,10 @@ package com.andrzejdubaj.rabbitmqdemoapp;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 
-public class RabbitMQMessageListener  implements MessageListener {
+public class RabbitMQMessageListener implements MessageListener {
 
     @Override
-    public void onMessage(Message message){
+    public void onMessage(Message message) {
         System.out.println("message = [" + new String(message.getBody()) + "]");
     }
 }
